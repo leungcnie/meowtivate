edce30646c7c516207a88a45b67d4b981a0a1762
agxcd <agxcdcode@gmail.com>
2021-01-15 14:27:42 -0800

    import react-calendar from open source

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import { Header } from "./stories/Header";
 import { Weather } from "./stories/Weather";
+import { CalendarApp } from "./stories/Calendar-import";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <CalendarApp />
       <Weather />
     </div>
   );
---
+++ front-end/src/stories/Calendar-import.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+
+import "react-modern-calendar-datepicker/lib/DatePicker.css";
+import { Calendar } from "react-modern-calendar-datepicker";
+import "./calendar.css";
+
+export const CalendarApp = () => {
+  const defaultValue = {
+    year: 2021,
+    month: 1,
+    day: 1,
+  };
+  const [selectedDay, setSelectedDay] = useState(defaultValue);
+
+  return (
+    <Calendar
+      value={selectedDay}
+      onChange={setSelectedDay}
+      shouldHighlightWeekends
+      customDaysClassName={[
+        // here we add some CSS classes
+        { year: 2021, month: 1, day: 4, className: "purpleDay" },
+        { year: 2021, month: 1, day: 12, className: "orangeDay" },
+        { year: 2021, month: 1, day: 18, className: "yellowDay" },
+        { year: 2021, month: 1, day: 26, className: "navyBlueDay" },
+      ]}
+    />
+  );
+};
