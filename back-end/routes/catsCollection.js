beb2475efaca3c6ffa723e262c17cb346a419ce9
agxcd <agxcdcode@gmail.com>
2021-01-16 23:40:06 -0800

    functional json display of cat collections ~~

--- back-end/routes/catsCollection.js
+++ back-end/routes/catsCollection.js
@@ -2,10 +2,14 @@ module.exports = (router, db) => {
   // Get collections with id
   router.get("/:id", (req, res) => {
     const user_id = req.params.id;
-    db.getCatsCollections(user_id).then((data) => {
-      console.log("cannot get the correct", data);
-    });
-    res.send("we got collection");
+    db.getCatsCollections(user_id)
+      .then((data) => {
+        res.json(data);
+        // console.log("cannot get the correct", data);
+      })
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
+      });
   });
 
   return router;
