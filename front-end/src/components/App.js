1b35ee79851c693b9241136a5f70289103861f02
monica-li205 <monicali@live.ca>
2021-01-17 14:37:15 -0800

    adds components and compiles a simple to-do list page

--- front-end/src/components/App.js
+++ front-end/src/components/App.js
@@ -1,20 +1,56 @@
-import "./styles/App.css";
-import { Header } from "./Header";
-import { Weather } from "./Weather";
-import { CalendarApp } from "./Calendar-import";
-
-import { Gallery } from "./Gallery";
-import collections from "./collections.json";
+import React from "react";
+import Button from "./myButton";
+import ToDoList from "./ToDoList";
 
 function App() {
+  const confirm = true;
+
   return (
-    <div className="App">
-      <Header />
-      <CalendarApp />
-      <Weather />
-      <Gallery items={collections} />
-    </div>
-  );
+    <main>
+      <header>
+        <h1>LET'S GET LOTS DONE TODAY!</h1>
+      </header>
+      <section>
+        <article>
+          <label>
+            Habit Tracker
+          </label>
+          <ToDoList/> 
+        </article>
+        <article>
+          <label>
+            To-Do Today
+          </label>
+          <ToDoList/>
+        </article>
+      </section>
+    <footer>
+      <Button confirm= { confirm } />
+    </footer>
+  </main>
+  )
 }
+
+// ReactDOM.render(<App />, document.querySelector("#app"));
+
+
+// import "./styles/App.css";
+// import { Header } from "./Header";
+// import { Weather } from "./Weather";
+// import { CalendarApp } from "./Calendar-import";
+
+// import { Gallery } from "./Gallery";
+// import collections from "./collections.json";
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <Header />
+//       <CalendarApp />
+//       <Weather />
+//       <Gallery items={collections} />
+//     </div>
+//   );
+// }
 
 export default App;
--- front-end/src/components/Button.js
+++ front-end/src/components/Button.js
@@ -1,50 +1,84 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import './styles/button.css';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
 
-/**
- * Primary UI component for user interaction
- */
-export const Button = ({ primary, backgroundColor, size, label, ...props }) => {
-  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+    },
+  },
+}));
+
+export default function ContainedButtons() {
+  const classes = useStyles();
+
   return (
-    <button
-      type="button"
-      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
-      style={backgroundColor && { backgroundColor }}
-      {...props}
-    >
-      {label}
-    </button>
+    <div className={classes.root}>
+      <Button variant="contained">Default</Button>
+      <Button variant="contained" color="primary">
+        Primary
+      </Button>
+      <Button variant="contained" color="secondary">
+        Secondary
+      </Button>
+      <Button variant="contained" disabled>
+        Disabled
+      </Button>
+      <Button variant="contained" color="primary" href="#contained-buttons">
+        Link
+      </Button>
+    </div>
   );
-};
+}
 
-Button.propTypes = {
-  /**
-   * Is this the principal call to action on the page?
-   */
-  primary: PropTypes.bool,
-  /**
-   * What background color to use
-   */
-  backgroundColor: PropTypes.string,
-  /**
-   * How large should the button be?
-   */
-  size: PropTypes.oneOf(['small', 'medium', 'large']),
-  /**
-   * Button contents
-   */
-  label: PropTypes.string.isRequired,
-  /**
-   * Optional click handler
-   */
-  onClick: PropTypes.func,
-};
+// import React from 'react';
+// import PropTypes from 'prop-types';
+// import './styles/button.css';
 
-Button.defaultProps = {
-  backgroundColor: null,
-  primary: false,
-  size: 'medium',
-  onClick: undefined,
-};
+// /**
+//  * Primary UI component for user interaction
+//  */
+// export const Button = ({ primary, backgroundColor, size, label, ...props }) => {
+//   const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
+//   return (
+//     <button
+//       type="button"
+//       className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
+//       style={backgroundColor && { backgroundColor }}
+//       {...props}
+//     >
+//       {label}
+//     </button>
+//   );
+// };
+
+// Button.propTypes = {
+//   /**
+//    * Is this the principal call to action on the page?
+//    */
+//   primary: PropTypes.bool,
+//   /**
+//    * What background color to use
+//    */
+//   backgroundColor: PropTypes.string,
+//   /**
+//    * How large should the button be?
+//    */
+//   size: PropTypes.oneOf(['small', 'medium', 'large']),
+//   /**
+//    * Button contents
+//    */
+//   label: PropTypes.string.isRequired,
+//   /**
+//    * Optional click handler
+//    */
+//   onClick: PropTypes.func,
+// };
+
+// Button.defaultProps = {
+//   backgroundColor: null,
+//   primary: false,
+//   size: 'medium',
+//   onClick: undefined,
+// };
