a1841d2d2d64b0664bcc505316ac36eaa5336317
monica-li205 <monicali@live.ca>
2021-01-18 15:53:38 -0800

    creates gallery grid container and cleans up files not being used

--- front-end/src/components/GalleryItem.js
+++ front-end/src/components/GalleryItem.js
@@ -1,18 +1,19 @@
 import React from "react";
+import Grid from '@material-ui/core/Grid';
 
-const GalleryItem = (props) => {
+export default function GalleryItem(props) {
   return (
-    <article className="meow-item">
-      <header className="meow-item-header">
-        <img className="meow-item-header-avatar" src={props.avatar} alt="" />
-        <h2 className="meow-item-header-name">{props.name}</h2>
-      </header>
-      <main className="meow-item-description">
-        <p>{props.description}</p>
-      </main>
-      <footer className="meow-item-footer">{props.date}</footer>
-    </article>
+    <Grid item xs={6} sm={3}>
+      <article className="meow-item">
+        <header className="meow-item-header">
+          <img className="meow-item-header-avatar" src={props.avatar} alt="" width="200px" />
+          <h2 className="meow-item-header-name">{props.name}</h2>
+        </header>
+        <main className="meow-item-description">
+          <p>{props.description}</p>
+        </main>
+        <footer className="meow-item-footer">{props.date}</footer>
+      </article>
+    </Grid>
   );
 };
-
-export default GalleryItem;
