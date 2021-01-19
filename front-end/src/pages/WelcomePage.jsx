f9f6320689bae543a2106f2c33f3dbddc20ab359
agxcd <agxcdcode@gmail.com>
2021-01-19 15:39:26 -0800

    need fix redirect

--- front-end/src/pages/WelcomePage.jsx
+++ front-end/src/pages/WelcomePage.jsx
@@ -1,9 +1,17 @@
 import NavBar from "../components/NavBar";
 import Button from "@material-ui/core/Button";
+import axios from "axios";
+import { Redirect } from "react-router";
 // import useApplicationData from "../hooks/useApplicationData";
 
 export default function WelcomePage(props) {
-  const { state } = props;
+  const login = () => {
+    axios.get("/api/login/1").then((res) => {
+      console.log(res);
+      res.redirect("/dashboard");
+    });
+  };
+
   return (
     <div className="Welcome">
       <NavBar />
@@ -11,8 +19,9 @@ export default function WelcomePage(props) {
       <Button variant="contained" color="primary">
         Sign Up
       </Button>
-      <Button variant="contained" color="secondary">
+      <Button variant="contained" color="secondary" onClick={login}>
         Login
+        {/* <Redirect to="/dashboard" />; */}
       </Button>
     </div>
   );
