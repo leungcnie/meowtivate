ddb65e9d5087af1c0215854781221d4b9bbb8f61
agxcd <agxcdcode@gmail.com>
2021-01-19 16:39:57 -0800

    404 image run no problem

--- front-end/src/components/404.jsx
+++ front-end/src/components/404.jsx
@@ -1,7 +1,10 @@
+import React from "react";
+import notfound from "../img/notfound.jpg";
+
 export default function NotFoundPage(props) {
   return (
     <main className="">
-      <img src="./img/notfound.jpg" alt="" />
+      <img src={notfound} alt="" />
       404 Not Found
     </main>
   );
