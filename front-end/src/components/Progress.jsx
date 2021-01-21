4ac773e8881e72a8e1d9a842b1ef4716470f988b
agxcd <agxcdcode@gmail.com>
2021-01-20 20:30:38 -0800

    progress bar set -

--- front-end/src/components/Progress.jsx
+++ front-end/src/components/Progress.jsx
@@ -1,28 +1,24 @@
 import React from "react";
-import styles from "./progress.css";
+import "./progress.css";
 
 function Progress(props) {
-  // const totalAmount = useAppState().items.length;
-  // const { completed } = useItems();
-  // const completedAmount = completed.length;
-
-  // let completedPercentage = completedAmount / totalAmount;
-
-  // if (isNaN(completedPercentage)) {
-  //   completedPercentage = 0;
-  // }
   const { items } = props;
+  const totalAmount = items.length;
+  const completed = items.filter((item) => (item.is_completed = true));
+  const completedAmount = completed.length;
+
+  const completedPercentage = completedAmount / totalAmount;
+
   console.log("props.items in progress", props.items);
   // const itemsCount = props.items;
 
   return (
-    <div className={styles.progress}>
+    <div className="progress">
       <div
-        className={`${styles.progressbar} ${styles.completed}`}
-        // style={{ width: `${completedPercentage * 100}%` }}
-      >
-        Hello {items.length}
-      </div>
+        className="completed"
+        style={{ width: `${completedPercentage * 100}%` }}
+      ></div>
+      Hello {completedAmount} / {totalAmount}
     </div>
   );
 }
