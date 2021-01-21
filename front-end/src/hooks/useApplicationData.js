87173513c1ba66db462f1eb7e29fba58a858c52a
Connie <leungcnie@gmail.com>
2021-01-21 01:21:28 -0800

    Rename lists to actions and create functions for deleting and adding actions

--- front-end/src/hooks/useApplicationData.js
+++ front-end/src/hooks/useApplicationData.js
@@ -1,39 +1,72 @@
 import { useEffect, useReducer, useState } from "react";
 import axios from "axios";
+import { removeFromActions, addToActions } from '../helpers/stateHelpers';
 
 export default function useApplicationDate() {
   const [state, setState] = useState({
     collections: [],
     todos: [],
     habits: [],
-    lists: [],
+    actions: [],
   });
 
-  console.log("useApplicationDate correct is_completed state", state.lists);
+  // console.log("useApplicationDate correct is_completed state", state.actions);
 
-  // const removeFromHabits = (id) => {
-  //   const target = state.habits.filter((obj) => {
-  //     obj.id === id
-  //   })[0];
+  const addAction = (action_name, categoryID) => {
+    console.log("action_name in useApp", action_name);
+    if (categoryID === 1) {
+      return axios.post('/api/todos/', { action_name })
+        .then((res) => {
+          const actions = addToActions(res.data, state); // res.data contains the action obj
+          const todos = actions.filter(obj => obj.category_id === 1);
 
-  //   const targetIndex = habits.indexOf(target);
+          console.log("updatedActions", actions);
+          // console.log("updatedHabits", habits);
+          console.log("updatedTodos", todos);
 
-  //   const habits = [...state.habits].slice()
-  // }
+          setState({
+            ...state,
+            actions,
+            todos
+          })
+        })
+    } else if (categoryID === 2) {
+      return axios.post('/api/habits/', { action_name })
+      .then((res) => {
+        const actions = addToActions(res.data, state);
+        const habits = actions.filter(obj => obj.category_id === 2);
 
-  const addAction = (actionName) => {};
+        console.log("updatedActions", actions);
+        console.log("updatedHabits", habits);
+        // console.log("updatedTodos", todos);
 
-  const deleteAction = (actionId) => {
-    // Delete habit/todo with actionId in the current state
+        setState({
+          ...state,
+          actions,
+          habits
+        })
+      })    
+    }
+  };
 
-    return axios.delete(`/api/actions/${actionId}`).then(() => {
+  const deleteAction = (actionID) => {
+    // Get updated actions
+    const actions = removeFromActions(actionID, state);
+    const habits = actions.filter(obj => obj.category_id === 2);
+    const todos = actions.filter(obj => obj.category_id === 1);
+
+    // Delete action in db and update state
+    return axios.delete(`/api/actions/${actionID}`).then(() => {
       setState({
         ...state,
+        todos: todos,
+        habits: habits,
+        actions: actions
       });
     });
   };
 
-  const editAction = (actionId) => {};
+  const editAction = (actionID) => {};
 
   const listFunctions = {
     addAction,
@@ -55,7 +88,7 @@ export default function useApplicationDate() {
           collections: res[0].data,
           todos: res[1].data,
           habits: res[2].data,
-          lists: res[3].data,
+          actions: res[3].data,
         }));
       })
       .catch((err) => {
@@ -63,5 +96,5 @@ export default function useApplicationDate() {
       });
   }, []);
 
-  return { state, deleteAction };
+  return { state, deleteAction, addAction };
 }
