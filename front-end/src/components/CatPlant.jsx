3aeffd9e5f42d513869900391dead83f9fbccad5
Connie <leungcnie@gmail.com>
2021-01-23 14:56:47 -0800

    Adjust animation speed

--- front-end/src/components/CatPlant.jsx
+++ front-end/src/components/CatPlant.jsx
@@ -22,7 +22,7 @@ const useStyles = makeStyles(theme => ({
 
   },
   animatedItem: {
-    animation: `$myEffect 3000ms ${theme.transitions.easing.easeInOut}`,
+    animation: `$myEffect 1500ms ${theme.transitions.easing.easeInOut}`,
 
   },
 
@@ -75,7 +75,7 @@ export default function CatPlant(props) {
   
   useEffect(() => {
     setStart(true);
-    timer = setTimeout(() => setStart(false), 3000);
+    timer = setTimeout(() => setStart(false), 1500);
   }, [actions])
 
   useEffect(() => {
