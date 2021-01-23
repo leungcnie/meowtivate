9618714c2ec5f63c3f61665e13f6b6f10e66c4b0
Connie <leungcnie@gmail.com>
2021-01-23 14:58:44 -0800

    Add basic styling to calendar

--- front-end/src/components/Calendar-import.js
+++ front-end/src/components/Calendar-import.js
@@ -5,25 +5,47 @@ import { Calendar } from "react-modern-calendar-datepicker";
 import "./styles/calendar.css";
 
 export const CalendarApp = () => {
-  const defaultValue = {
+  const defaultFrom = {
     year: 2021,
     month: 1,
-    day: 1,
+    day: 20,
   };
-  const [selectedDay, setSelectedDay] = useState(defaultValue);
+  const defaultTo = {
+    year: 2021,
+    month: 1,
+    day: 23,
+  };
+  const defaultValue = {
+    from: defaultFrom,
+    to: defaultTo,
+  };
+
+  const [selectedDayRange, setSelectedDayRange] = useState(
+    defaultValue
+  );
 
   return (
     <Calendar
-      value={selectedDay}
-      onChange={setSelectedDay}
+      value={selectedDayRange}
+      onChange={setSelectedDayRange}
+      // colorPrimary="#0fbcf9" // Range start and end
+      // colorPrimaryLight="rgba(75, 207, 250, 0.4)" // Range middle
+      calendarTodayClassName="custom-today-day"
       shouldHighlightWeekends
-      customDaysClassName={[
+      // customDaysClassName={[
         // here we add some CSS classes
         // { year: 2021, month: 1, day: 4, className: "purpleDay" },
         // { year: 2021, month: 1, day: 12, className: "orangeDay" },
         // { year: 2021, month: 1, day: 18, className: "yellowDay" },
         // { year: 2021, month: 1, day: 26, className: "navyBlueDay" },
-      ]}
+      // ]}
+      renderFooter={() => (
+        <div 
+          style={{ display: 'flex', justifyContent: 'center', padding: '1rem 2rem' }}
+          className="footer">
+          🌱 CURRENT STREAK
+        </div>
+      )}
     />
   );
 };
