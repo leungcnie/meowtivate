3169cf68129c8cf17eda28b33d4309c3db288061
Connie <leungcnie@gmail.com>
2021-01-23 20:46:34 -0800

    Create mock data for 3 day progression

--- back-end/db/seeds/06_actions_seeds.sql
+++ back-end/db/seeds/06_actions_seeds.sql
@@ -4,3 +4,12 @@ VALUES (1, 1, 'Buy groceries', CURRENT_DATE, true),
 (1, 1, 'Go for a walk', CURRENT_DATE, false),
 (1, 2, 'Drink a liter of water', CURRENT_DATE, false),
 (1, 2, '10 mins of exercise', CURRENT_DATE, true);
+
+INSERT INTO actions (user_id, category_id, action_name, date_created, is_completed)
+VALUES (2, 2, 'Drink a liter of water', CURRENT_DATE, false),
+(2, 2, '15 mins of exercise', CURRENT_DATE, true);
+
+INSERT INTO actions (user_id, category_id, action_name, date_created, is_completed)
+VALUES (3, 2, 'Drink a liter of water', CURRENT_DATE, false),
+(3, 2, '15 mins of exercise', CURRENT_DATE, true),
+(2, 1, 'Run a marathon', CURRENT_DATE, false);
