2fc3beb30bea4028e5a480250186a8f734082f0d
agxcd <agxcdcode@gmail.com>
2021-01-25 18:23:23 -0800

    style register

--- front-end/src/pages/RegisterPage.jsx
+++ front-end/src/pages/RegisterPage.jsx
@@ -2,52 +2,57 @@ import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 import { makeStyles } from "@material-ui/core/styles";
 import { Link } from "react-router-dom";
-import DoubleArrowRoundedIcon from '@material-ui/icons/DoubleArrowRounded';
+import DoubleArrowRoundedIcon from "@material-ui/icons/DoubleArrowRounded";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     fontFamily: "Varela Round",
-    '& > *': {
+    "& > *": {
       margin: theme.spacing(3),
     },
   },
   container: {
-    display: 'inline-flex',
-    justifyContent: 'center',
-    flexDirection: 'column',
-    backgroundColor: 'rgb(201,188,200, 0.4)',
-    borderRadius: '2rem',
-    width: '80vw',
-    paddingTop: '5em',
-    paddingBottom: '5em',
-    marginTop: '15vh'   
+    display: "inline-flex",
+    justifyContent: "center",
+    flexDirection: "column",
+    backgroundColor: "rgb(201,188,200, 0.4)",
+    borderRadius: "2rem",
+    [theme.breakpoints.down("sm")]: {
+      fontSize: "2em",
+      height: "60vh",
+    },
+    width: "80vw",
+    paddingTop: "5em",
+    paddingBottom: "5em",
+    marginTop: "15vh",
   },
   logo: {
     fontFamily: "itim",
     letterSpacing: "8px",
-    fontSize: '4em',
-    color: 'antiquewhite',
-    lineHeight: '0',
+    fontSize: "4em",
+
+    color: "antiquewhite",
+    lineHeight: "0",
   },
   header: {
     fontFamily: "Varela Round",
     letterSpacing: "6px",
-    color: 'grey',
+    color: "grey",
   },
   loginButton: {
     fontFamily: "Varela Round",
 
-    backgroundColor: '#a0cdca',
-    '&:hover': {
-      backgroundColor: '#78aca8',
-    }
+    backgroundColor: "#a0cdca",
+    "&:hover": {
+      backgroundColor: "#78aca8",
+    },
   },
   icon: {
-    color: 'white',
-    fontSize: '2.7em',
-    paddingLeft: '1rem',
-    paddingRight: '1rem',
-  }
+    color: "white",
+    fontSize: "2.7em",
+    paddingLeft: "1rem",
+    paddingRight: "1rem",
+  },
 }));
 
 export default function RegisterPage(props) {
@@ -72,12 +77,13 @@ export default function RegisterPage(props) {
           <TextField id="outlined-basic" label="Password" variant="outlined" />
         </div>
         <Button
-        className={classes.loginButton}
-        variant="contained" 
-        component={Link}
-        to='/dashboard'>
-        <DoubleArrowRoundedIcon className={classes.icon}/>
-      </Button>
+          className={classes.loginButton}
+          variant="contained"
+          component={Link}
+          to="/dashboard"
+        >
+          <DoubleArrowRoundedIcon className={classes.icon} />
+        </Button>
       </form>
     </div>
   );
--- front-end/src/pages/WelcomePage.jsx
+++ front-end/src/pages/WelcomePage.jsx
@@ -1,69 +1,71 @@
-import axios from "axios";
-import { useHistory } from "react-router-dom";
 import { makeStyles } from "@material-ui/core/styles";
 import { Link } from "react-router-dom";
 import Button from "@material-ui/core/Button";
-import LocalFloristIcon from '@material-ui/icons/LocalFlorist';
-import EcoIcon from '@material-ui/icons/Eco';
+import LocalFloristIcon from "@material-ui/icons/LocalFlorist";
+import EcoIcon from "@material-ui/icons/Eco";
 
 const useStyles = makeStyles((theme) => ({
   icon: {
-    fontSize: '1.3em',
+    fontSize: "1.3em",
   },
   container: {
-    display: 'inline-flex',
-    justifyContent: 'center',
-    flexDirection: 'column',
-    backgroundColor: 'rgb(201,188,200, 0.4)',
-    borderRadius: '2rem',
-    width: '80vw',
-    paddingTop: '5em',
-    paddingBottom: '5em',
-    marginTop: '15vh'   
+    display: "inline-flex",
+    justifyContent: "center",
+    flexDirection: "column",
+    backgroundColor: "rgb(201,188,200, 0.4)",
+    borderRadius: "2rem",
+    width: "80vw",
+    paddingTop: "5em",
+    paddingBottom: "5em",
+    marginTop: "15vh",
   },
   logo: {
     fontFamily: "itim",
     letterSpacing: "8px",
-    fontSize: '5em',
-    lineHeight: '0',
-    color: '#5c5c5c'
+    fontSize: "5em",
+    [theme.breakpoints.down("sm")]: {
+      fontSize: "2em",
+    },
+    lineHeight: "0",
+    color: "#5c5c5c",
   },
   header: {
     fontFamily: "Varela Round",
     letterSpacing: "6px",
-    fontSize: '3em',
-    color: 'grey',
-    lineHeight: '0'
+    fontSize: "3em",
+    [theme.breakpoints.down("sm")]: {
+      fontSize: "2em",
+    },
+    color: "grey",
+    lineHeight: "0",
   },
   signUpButton: {
     fontFamily: "Varela Round",
-    margin: '2rem',
-    backgroundColor: '#ebcb8a',
-    fontSize: '2em',
-    '&:hover': {
-      backgroundColor: '#ceba93',
-    }
+    margin: "2rem",
+    backgroundColor: "#ebcb8a",
+    [theme.breakpoints.down("sm")]: {
+      fontSize: "1em",
+    },
+    fontSize: "2em",
+    "&:hover": {
+      backgroundColor: "#ceba93",
+    },
   },
   loginButton: {
     fontFamily: "Varela Round",
-    margin: '2rem',
-    fontSize: '2em',
-    backgroundColor: '#a0cdca',
-    '&:hover': {
-      backgroundColor: '#78aca8',
-    }
+    margin: "2rem",
+    fontSize: "2em",
+    [theme.breakpoints.down("sm")]: {
+      fontSize: "1em",
+    },
+    backgroundColor: "#a0cdca",
+    "&:hover": {
+      backgroundColor: "#78aca8",
+    },
   },
 }));
 
 export default function WelcomePage(props) {
-  let history = useHistory();
-
-  const login = () => {
-    axios.get("/api/login/1").then((res) => {
-      // console.log(res);
-      history.push("/dashboard");
-    });
-  };
   // const { state } = props;
   const classes = useStyles();
 
@@ -79,7 +81,7 @@ export default function WelcomePage(props) {
           component={Link}
           to="/register"
         >
-          <EcoIcon className={classes.icon}/>
+          <EcoIcon className={classes.icon} />
           Sign Up
         </Button>
         <Button
@@ -89,7 +91,7 @@ export default function WelcomePage(props) {
           component={Link}
           to="/login"
         >
-        <LocalFloristIcon className={classes.icon}/>
+          <LocalFloristIcon className={classes.icon} />
           Login
         </Button>
       </div>
