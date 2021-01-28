07b53a35fdfb8fd21f98abe3b1648ef59b1d3bf2
Connie <leungcnie@gmail.com>
2021-01-27 16:52:57 -0800

    Update pot names and descriptions

--- back-end/db/seeds/07_pots_seeds.sql
+++ back-end/db/seeds/07_pots_seeds.sql
@@ -1,8 +1,7 @@
 INSERT INTO pots (pot_name, description, image_url, price)
 VALUES ('Basic Pot', 'Nothing flashy, but it''ll certainly do the job.', 'https://meowtivate.s3-us-west-2.amazonaws.com/pots/01pot.png', 100),
--- ('Basic Pot', 'You already have one of these. Why waste your points?', 'https://meowtivate.s3-us-west-2.amazonaws.com/pots/01pot.png', 100, false),
 ('Serene Pot', 'Very fragile. Make sure your cat doesn''t get to it.', 'https://meowtivate.s3-us-west-2.amazonaws.com/pots/02pot.png', 700),
 ('Pink Pot', 'so Aesthetique.', 'https://meowtivate.s3-us-west-2.amazonaws.com/pots/03pot.png', 400),
-('Pink Pot', 'so Aesthetique.', 'https://meowtivate.s3-us-west-2.amazonaws.com/pots/04pot.png', 400),
-('Pink Pot', 'so Aesthetique.', 'https://meowtivate.s3-us-west-2.amazonaws.com/pots/05pot.png', 400),
-('Pink Pot', 'so Aesthetique.', 'https://meowtivate.s3-us-west-2.amazonaws.com/pots/06pot.png', 400);
+('Terracotta Pot', 'A timeless classic. Good for plants that enjoy drier soil.', 'https://meowtivate.s3-us-west-2.amazonaws.com/pots/04pot.png', 100),
+('Colour Block Pot', 'They say the stripe is pure gold. But then again, they say a lot of things.', 'https://meowtivate.s3-us-west-2.amazonaws.com/pots/05pot.png', 800),
+('Leggy Pot', 'Legs for days! Industrial with a modern twist.', 'https://meowtivate.s3-us-west-2.amazonaws.com/pots/06pot.png', 400);
